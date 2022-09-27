c6f2d0ca7f6c417135f326de74c36837640afae0
Denis Fomin <fo.denis2011@gmail.com>
2022-09-28 00:15:37 +0300

    (feat) add support for evaluating bang expressions

--- src/evaluator/mod.rs
+++ src/evaluator/mod.rs
@@ -3,6 +3,28 @@ pub mod objects;
 use objects::*;
 use crate::ast::*;
 
+fn eval_bang_expression(right: EvalObject) -> EvalObject {
+    match right {
+        EvalObject::Boolean(b) => {
+            match b {
+                true => EvalObject::Boolean(false),
+                false => EvalObject::Boolean(true)
+            }
+        },
+        EvalObject::Null => EvalObject::Boolean(true),
+        _ => EvalObject::Boolean(false)
+    }
+}
+
+fn eval_prefix_expression(prefix: Prefix, right: EvalObject) -> EvalObject {
+    match prefix {
+        Prefix::Bang => {
+            eval_bang_expression(right)
+        },
+        _ => unimplemented!()
+    }
+}
+
 pub fn eval(node: Node) -> EvalObject {
     match node {
         Node::Program(program) => {
@@ -11,13 +33,25 @@ pub fn eval(node: Node) -> EvalObject {
                 return eval(Node::Stmt(stmt))
             }
         },
-        Node::Stmt(stmt) => {
-            match stmt {
-                Stmt::Expr(Expr::Literal(Literal::Int(i))) => {
+        Node::Expr(expr) => {
+            match expr {
+                Expr::Literal(Literal::Int(i)) => {
                     return EvalObject::Int(i.try_into().unwrap());
                 },
-                Stmt::Expr(Expr::Literal(Literal::Boolean(b))) => {
+                Expr::Literal(Literal::Boolean(b)) => {
                     return EvalObject::Boolean(b);
+                },
+                Expr::Prefix(prefix, pexpr) => {
+                    let right = eval(Node::Expr(*pexpr));
+                    return eval_prefix_expression(prefix, right)
+                },
+                _ => unimplemented!()
+            }
+        },
+        Node::Stmt(stmt) => {
+            match stmt {
+                Stmt::Expr(expr) => {
+                    return eval(Node::Expr(expr))
                 },
                 _ => unimplemented!(),
             }
@@ -68,6 +102,25 @@ mod test {
         for (input, expected_result) in test_cases {
             match run_eval(input.to_string()) {
                 EvalObject::Boolean(b) => assert_eq!(b, expected_result),
+                _ => panic!("Expect a Boolean type!")
+            }
+        }
+    }
+
+    #[test]
+    fn test_bang_operator() {
+        let test_cases = vec![
+            ("!true;", false),
+            ("!false;", true),
+            ("!5;", false),
+            ("!!true;", true),
+            ("!!false;", false),
+            ("!!5;", true),
+        ];
+
+        for (input, expected_result) in test_cases {
+            match run_eval(input.to_string()) {
+                EvalObject::Boolean(b) => assert_eq!(b, expected_result, "{}", input),
                 _ => panic!("Expect a Boolean type!")
             }
         }
